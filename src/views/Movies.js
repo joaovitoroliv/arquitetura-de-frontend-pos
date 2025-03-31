db7dadb2d053aaa726d27335b27c688869bce3d0
João Vitor Oliveira Souza <joaovitoroliveirasouza@MacBook-Air-de-Joao.local>
2025-03-31 20:47:03 -0300

    fix: modifica view de listagem de filmes para lidar com os dados extraidos do service

--- src/views/Movies.js
+++ src/views/Movies.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { getPopularMovies } from "../services/movies.services";
+
 export function Movies() {
-    return <h1>
-        Movies page
-    </h1>
+    // Criar um estado pois ele irá mudar - Vai começar com uma lista vazia e quando ela for executada em paralelo (Promise)
+    const [movies, setMovies] = useState([]); // Renderizar inicialmente com vazio
+
+    // Sei que dentro do objeto do Axios tenho uma propriedade chamada data, entao posso usar destructuring (desestruturação)
+    useEffect(() => {
+      getPopularMovies().then(({data}) => {
+        setMovies(data.results)
+      })
+    }, [])
+
+    return (
+    <section>
+        <h1>
+            Filmes populares
+
+            <ul>
+                {movies.map((movie) => <li>{movie.title}</li>)}
+            </ul>
+        </h1>
+    </section>);
+    
 }
